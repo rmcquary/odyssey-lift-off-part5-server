230681236460110ed34bca7a5bd71d3e6519cc00
Rebecca McQuary <rebecca.mcquary@gmail.com>
2022-07-08 10:49:01 -0500

    updating schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,11 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    durationInSections: ({ length }) => length,
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
